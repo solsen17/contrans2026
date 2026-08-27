4e30b1deb241f6dbc1da26b305d510c3dfcfb905
Stephen Olsen <solsen17@vt.edu>
2026-08-27 15:18:38 -0400

    Built a dashboard with many problems to better understand what data engineering is all about

---
+++ skeleton/app.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import requests
+import plotly.express as px
+from dash import Dash, html, dcc
+
+url = 'https://api.congress.gov/v3/member'
+params = {'api_key': 'DEMO_KEY'}
+r = requests.get(url, params = params)
+
+memberdata = pd.json_normalize(r.json(), record_path=['members'])
+
+member_parties = memberdata.groupby('partyName').size().reset_index()
+member_parties = member_parties.rename({0:'count'}, axis=1)
+
+fig = px.bar(member_parties, x='partyName', y='count')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+
+    html.H1('Congress Transparency Dashboard'),
+    dcc.Markdown('This is a very very bad website'),
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
